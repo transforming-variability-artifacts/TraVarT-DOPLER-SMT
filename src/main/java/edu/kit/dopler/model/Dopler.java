572a05e208ac974b8a412168b0183ad9dcae84aa
FabianEger <fabianeger@gmx.de>
2024-05-01 17:18:51 +0200

    added constructor and getter/setter to the Dopler.java

--- src/main/java/edu/kit/dopler/model/Dopler.java
+++ src/main/java/edu/kit/dopler/model/Dopler.java
@@ -9,4 +9,42 @@ public class Dopler {
     Set<Assets> assets = Collections.emptySet();
     Set<Enum> enumSet = Collections.emptySet();
 
+    public Dopler(Set<IDecisionType> decisions, Set<Assets> assets, Set<Enum> enumSet) {
+        this.decisions = decisions;
+        this.assets = assets;
+        this.enumSet = enumSet;
+    }
+
+
+    public void addDecision(IDecisionType decisionType){
+        decisions.add(decisionType);
+    }
+
+    public void removeDecision(IDecisionType decisionType){
+        decisions.remove(decisionType);
+    }
+
+    public Set<IDecisionType> getDecisions() {
+        return decisions;
+    }
+
+    public void setDecisions(Set<IDecisionType> decisions) {
+        this.decisions = decisions;
+    }
+
+    public Set<Assets> getAssets() {
+        return assets;
+    }
+
+    public void setAssets(Set<Assets> assets) {
+        this.assets = assets;
+    }
+
+    public Set<Enum> getEnumSet() {
+        return enumSet;
+    }
+
+    public void setEnumSet(Set<Enum> enumSet) {
+        this.enumSet = enumSet;
+    }
 }
