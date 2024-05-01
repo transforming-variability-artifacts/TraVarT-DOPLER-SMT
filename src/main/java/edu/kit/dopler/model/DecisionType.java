04c24277be7c503dfd4b06a4d3d70473c7598796
FabianEger <fabianeger@gmx.de>
2024-05-01 17:17:48 +0200

    changes to the DecisionType

--- src/main/java/edu/kit/dopler/model/DecisionType.java
+++ src/main/java/edu/kit/dopler/model/DecisionType.java
@@ -7,11 +7,15 @@ abstract class DecisionType implements IDecisionType {
     private String question;
     private String description;
     private IExpression visibilityCondition;
+    private Set<Rule> rules;
+    private boolean taken;
 
-    public DecisionType(String question, String description, IExpression visibilityCondition) {
+    public DecisionType(String question, String description, IExpression visibilityCondition, boolean taken,Set<Rule> rules) {
         this.question = question;
         this.description = description;
         this.visibilityCondition = visibilityCondition;
+        this.taken = taken;
+        this.rules = rules;
     }
 
 
@@ -37,17 +41,17 @@ abstract class DecisionType implements IDecisionType {
 
     @Override
     public Set<Rule> getRules() {
-        return Set.of();
+        return rules;
     }
 
     @Override
     public void addRule(Rule rule) {
-
+        rules.add(rule);
     }
 
     @Override
     public void removeRule(Rule rule) {
-
+        rules.remove(rule);
     }
 
     @Override
@@ -58,5 +62,14 @@ abstract class DecisionType implements IDecisionType {
     @Override
     public void setVisibilityCondition(IExpression visibilityCondition) {
         this.visibilityCondition = visibilityCondition;
+    }
+
+
+    public boolean isTaken() {
+        return taken;
+    }
+
+    public void setTaken(boolean taken) {
+        this.taken = taken;
     }
 }
