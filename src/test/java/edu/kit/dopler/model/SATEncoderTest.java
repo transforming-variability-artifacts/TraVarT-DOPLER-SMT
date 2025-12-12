0610a3854dff0199b708d2020f280629b36bc4d6
uqrjd <uqrjd@student.kit.edu>
2025-12-12 09:49:26 +0100

    removed Feature IDE tests

--- src/test/java/edu/kit/dopler/model/SATEncoderTest.java
+++ src/test/java/edu/kit/dopler/model/SATEncoderTest.java
@@ -23,13 +23,8 @@ import static edu.kit.dopler.common.SolverUtils.checkSat;
 import static edu.kit.dopler.common.SolverUtils.getAmountOfConfigs;
 
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 import java.util.stream.Stream;
 
 //
